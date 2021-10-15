3ff8806cb0103d31c7a65950e35d2856b53635ab
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-10-14 21:47:20 -0300

    Fixes fdisplay delay

--- modelsim/ledsshow_tb.v
+++ modelsim/ledsshow_tb.v
@@ -12,7 +12,7 @@ module ledsshow_tb;
     );
 
     initial  begin
-        $dumpfile("./log/report.vcd");
+        $dumpfile("./log/report2.vcd");
         $dumpvars(0,ledsshow_tb);
         // this only will take effect after perform a "restart"
         // what will make desapper the wave form, I know...
@@ -21,43 +21,63 @@ module ledsshow_tb;
 
     initial begin
         $display("running tests");
-        fileLog = $fopen("./log/report.log");
+        fileLog = $fopen("./log/report2.log");
         registerData;
 
         #1 // 1ps
         pushButton = 1'b0;
-        registerData;
 
         #1 // 2ps
-        pushButton = ~pushButton;
         registerData;
 
         #1 // 3ps
-        registerData;
+        pushButton = ~pushButton;
 
         #1 // 4ps
-        pushButton = ~pushButton;
         registerData;
 
         #1 // 5ps
-        registerData;
 
         #1 // 6ps
-        pushButton = ~pushButton;
         registerData;
 
         #1 // 7ps
-        registerData;
+        pushButton = ~pushButton;
 
         #1 // 8ps
         registerData;
 
         #1 // 9ps
-        pushButton = ~pushButton;
-        registerData;
 
         #1 // 10ps
+        registerData;
+
+        #1 // 11ps
         pushButton = ~pushButton;
+
+        #1 // 12ps
+        registerData;
+
+        #1 // 13ps
+
+        #1 // 14ps
+        registerData;
+
+        #1 // 15ps
+
+        #1 // 16ps
+        registerData;
+
+        #1 // 17ps
+        pushButton = ~pushButton;
+
+        #1 // 18ps
+        registerData;
+
+        #1 // 19ps
+        pushButton = ~pushButton;
+
+        #1 // 20ps
         registerData;
 
         $fclose(fileLog);
